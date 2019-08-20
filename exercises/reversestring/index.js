e3c471b5c0ccb15522a439f6ede8fbd7f27c582c
akramhelil <akramhelil@gmail.com>
2019-08-20 19:53:16 -0400

    reversed string 3 solution added

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // ES2015 Arrow funtion solution 
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+
+// function reverse(str) {
+    // my solution 
+    // const reversed = str.split('').reverse().join('');
+    // return reversed;
+
+    // solution #1 
+    // const arr = str.split('');
+    // arr.reverse();
+    // return arr.join('');
+
+    // solition #2 
+    // return str
+    //     .split('')
+    //     .reverse()
+    //     .join('');
+
+    //solution 3 
+    // new ES2015 syntax
+    // let reversed = '';
+    // for (let character of str) {
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+    
+// }

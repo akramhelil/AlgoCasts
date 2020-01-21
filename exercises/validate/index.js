43ef5b1e022efa34828359f5dbbf5d519541a8a9
akramhelil <akramhelil@gmail.com>
2020-01-21 14:42:32 -0500

    Done

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -7,9 +7,24 @@
 
 function validate(node, min = null, max = null) {
     // recursion to pass through  the information 
+    // recursion to pass through  the information 
     if (max !== null && node.data > max) {
-        return false 
+        return false;
     }
+
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    //  Validate funtion calls will be the one that will do the recursive funtion calls 
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    
+    if (node.right && !vaildate(node.right, node.data, max)) {
+        return false;
+    }
+
+    return true;
 }
 
 module.exports = validate;
